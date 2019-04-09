316083a6fae8d723327974283be4e08aef94ff77
yiwen1003 <yiwen.chen1003@gmail.com>
2019-04-09 15:39:03 +0800

    add Hellow.py

---
+++ Hellow.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+def hellow1():
+    print("Hellow World!")
+
+def hellow2():
+    print("Hi world~")
+
+hellow2()
+
+if __name__ == '__main__':
+    hellow1()
+
